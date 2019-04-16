3fe6a3c480dc6b91f8e1359dea2c67f0e857e456
clodx5 <clacli00@gmail.com>
2019-04-16 09:27:01 +0000

    06.1

--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -7,24 +8,22 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'card';
-    votes: number;
-    title: string;
-    link: string;
+    article:Article
 
     constructor() {
-      this.title = 'Angular 2';
-      this.link = 'http://angular.io';
-      this.votes = 10;
+      this.article = new Article('Angular 2','http://angular.io',10);
     }
 
-    voteUp():Boolean { //Aggiunto tipo di ritorno
-    this.votes += 1;
-    return false; //Non propagare l'evento
+    voteUp(): Boolean {
+      this.article.voteUp(); //Modificato qui
+      return false;
     }
-    voteDown():Boolean{
-        this.votes -= 1;
-        return false; //Non propagare l'evento
+
+    voteDown():Boolean {
+      this.article.voteDown(); //Modificato qui
+      return false;
     }
+
 
     ngOnInit() {}
 
