190f11fefc32501a6bc01eabcf39f6e0a503f099
clodx5 <clacli00@gmail.com>
2019-04-25 09:59:52 +0000

    16.1

--- angular-reddit_http/src/app/app.component.ts
+++ angular-reddit_http/src/app/app.component.ts
@@ -9,14 +9,14 @@ import { Article } from './article/article.model';
 export class AppComponent {
   articles:Article[];   // <-- component property
   constructor(){
-    this.articles = [
+   /* this.articles = [
       new Article('Angular 2', 'http://angular.io', 3),
       new Article('Fullstack', 'http://fullstack.io', 2),
       new Article('Angular Homepage', 'http://angular.io', 1),
-    ];
+    ];*/
   }
 
-  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+ /* addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     this.articles.push(new Article(title.value, link.value, 0));
     title.value = '';
@@ -26,6 +26,6 @@ export class AppComponent {
 
   sortedArticles(): Article[] {
     return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
-  }
+  } */
 
 }
