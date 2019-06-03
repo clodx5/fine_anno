d92cf6a9f38aa2bce37a58909b464faabfb7f514
clodx5 <clacli00@gmail.com>
2019-06-03 11:55:12 +0000

    15

--- angular-noleggio/src/app/app.component.ts
+++ angular-noleggio/src/app/app.component.ts
@@ -25,6 +25,8 @@ export class AppComponent {
   public err: boolean;
 
   public username = "";
+  public dataora = new Date();
+  public time = '';
 
   constructor(public http: HttpClient) {}
 
@@ -73,17 +75,21 @@ export class AppComponent {
   makeLogin(user: HTMLInputElement, pw: HTMLInputElement): boolean {
       //post a server node per verificare credenziali
     this.login(user.value, pw.value);
+
+    //var username usata per test funzionamento mostra/nascondi
+    this.username = user.value;
+
     return false;
   }
 
   login(user: string, password: string): void {
-      const headers = new HttpHeaders({
+    const headers = new HttpHeaders({
        'Content-Type': 'application/x-www-form-urlencoded'
     });
 
    const params = new HttpParams()
-    .set('nome', user)
-    .set('cognome', password);
+    .set('username', user)
+    .set('password', password);
 
     const options = {
       headers,
@@ -104,23 +110,72 @@ export class AppComponent {
 
   //noleggio
   makeNoleggio(idmono: HTMLInputElement): boolean {
-      this.noleggio(idmono.value); //aggiungere id utente, ora, etc
+      this.dataora = new Date();
+      this.time = this.dataora.toString();
+      this.noleggio(idmono.value, this.username, this.time);
       return false;
   }
 
-  noleggio(idmono:string): void {
+  noleggio(idmono:string, user:string, time:string): void {
+    const headers = new HttpHeaders({
+       'Content-Type': 'application/x-www-form-urlencoded'
+    });
 
+   const params = new HttpParams()
+    .set('id_mono', idmono)
+    .set('id_user', user)
+    .set('dataora', time);
+
+    const options = {
+      headers,
+      params,
+      withCredentials: false
+    };
+
+    this.http.post('http://node19.codenvy.io:36436/noleggio', null, options)
+     .subscribe(data => {
+       this.data = data;
+        if(data == true){
+          alert("ok");
+       }else{
+          alert("err");
+       }
+     });
   }
 
 
   //riconsegna
   makeRicon(idmono: HTMLInputElement): boolean {
-      this.riconsegna(idmono.value);
+      this.dataora = new Date();
+      this.time = this.dataora.toString();
+      this.riconsegna(idmono.value, this.time);
       return false;
   }
 
-  riconsegna(idmono:string): void {
+  riconsegna(idmono:string, time:string): void {
+    const headers = new HttpHeaders({
+       'Content-Type': 'application/x-www-form-urlencoded'
+    });
 
+   const params = new HttpParams()
+    .set('id_mono', idmono)
+    .set('dataora', time);
+
+    const options = {
+      headers,
+      params,
+      withCredentials: false
+    };
+
+    this.http.post('http://node19.codenvy.io:36436/riconsegna', null, options)
+     .subscribe(data => {
+       this.data = data;
+        if(data == true){
+          alert("ok");
+       }else{
+          alert("err");
+       }
+     });
   }
 
   //segnalazione
